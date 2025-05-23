e6cf8bb315bfbd988198b9699d76e5230cf0606a
vscardozo <vitoria.cardoso31@escola.pr.gov.br>
2025-05-23 14:10:53 +0000

    oi

--- atividade.js
+++ atividade.js
@@ -1,1 +1,41 @@
+let name = "vitória";
+console.log(`Olá, meu nome é ${name}.`);
 
+let idade = "17";
+let cidade= "Almirante Tamandaré";
+console.log(`Tenho ${idade} anos e moro em ${cidade}`);
+
+let prato_um = "strogonoff"
+let prato_dois = "macarrão"
+let prato_tres = "sobremesa de maracujá"
+console.log(`Minhas comidas favoritas são: ${prato_um}, ${prato_dois} e ${prato_tres}.`)
+
+let preco = 35.90;
+console.log(`O produto custa R$ ${preco}.`);
+
+let produto = "caneta";
+let quantidade = 5;
+
+console.log(`Comprei ${quantidade} unidades de ${produto}.`);
+
+let temDesconto = true;
+console.log(`Tem desconto disponível? ${temDesconto}`);
+
+let anoAtual = 2025;
+let anoNascimento = 2008;
+let idadeCalculada = anoAtual - anoNascimento;
+console.log(`Você tem ${idade} anos.`);
+
+let curso = "Engenharia de Software";
+let instituicao = "Universidade Tecnológica";
+
+console.log(`Estou cursando ${curso} no ${instituicao}.`);
+
+let cor1 = "azul";
+let cor2 = "vermelho";
+let cor3 = "roxo";
+
+console.log(`Minhas cores preferidas são ${cor1}, ${cor2} e ${cor3}.`);
+
+let mensagem = "Olá! meu nome é vitória.";
+console.log(mensagem);
